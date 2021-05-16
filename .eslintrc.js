6528287c0ee14229c6f9ac1a61bb4c1d32eb654b
jscheol <jsc@smartauth.kr>
2021-05-16 22:27:20 +0900

    style: eslint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,19 @@ module.exports = {
         es6: true,
         node: true,
     },
-    extends: ['airbnb', 'plugin:prettier/recommended'],
+    extends: ['airbnb', 'eslint:recommended', 'plugin:prettier/recommended'],
+    rules: {
+        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+        "import/no-unresolved": "off",
+        'import/extensions': [
+            "error",
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+                json: 'always'
+            },
+        ],
+    },
 };
